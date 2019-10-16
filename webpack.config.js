e06c786b26f02705dd16c86e5a7b8c16cffbf28f
Drekavac <diesel39931@gmail.com>
2019-10-17 04:58:40 +0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        hot: true,
+        compress: true,
+        port: 9000,
+        watchContentBase: true,
+        progress: true,
+        stats: {
+            cached: false
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+};
